973c4232dc8ef32b6d38284ab350ed5598e08b2b
tomhanlon <tomhanlon@gmail.com>
2015-02-02 20:52:24 -0600

    Complete function

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,8 +1,10 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
 define(function() {
   return {
-    fizzBuzz : function(num) {
+    fizzBuzz: function(num) {
       // write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
       // if the number is divisible by 5, the function should return 'buzz';
@@ -11,6 +13,16 @@ define(function() {
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      // for (var i = 0; i < num; i++) {
+
+      // Verify type
+      if (typeof num === 'number') {
+        var output = '';
+        if (num % 3 === 0) output += 'fizz';
+        if (num % 5 === 0) output += 'buzz';
+        if (output === '') output = num;
+        return output;
+      }
     }
   };
 });
