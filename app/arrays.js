0df770f27cf81fc18bcb0802531bc06c0a2808a9
tomhanlon <tomhanlon@gmail.com>
2015-02-02 21:46:57 -0600

    Finish section

--- app/arrays.js
+++ app/arrays.js
@@ -1,61 +1,118 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
 define(function() {
   return {
-    indexOf : function(arr, item) {
-
+    indexOf: function(arr, item) {
+      return arr.indexOf(item);
     },
 
-    sum : function(arr) {
-
+    sum: function(arr) {
+      var counter = 0;
+      for (var i = 0; i < arr.length; i++) {
+        counter += arr[i];
+      }
+      return counter;
     },
 
-    remove : function(arr, item) {
-
+    remove: function(arr, item) {
+      // Find all indices of element in array
+      for (var i = 0; i < arr.length; i++) {
+        // If item in array is equal to the item we want to remove
+        if (arr[i] === item) {
+          // Remove that element from array
+          arr.splice(i, 1);
+        }
+      }
+      return arr;
     },
 
-    removeWithoutCopy : function(arr, item) {
-
+    removeWithoutCopy: function(arr, item) {
+      var length = arr.length;
+      for (var i = 0; i < length; i++) {
+        if (arr[i] === item) {
+          arr.splice(i, item);
+        }
+      }
+      return arr;
     },
 
-    append : function(arr, item) {
-
+    append: function(arr, item) {
+      arr.push(item);
+      return arr;
     },
 
-    truncate : function(arr) {
-
+    truncate: function(arr) {
+      arr.pop();
+      return arr;
     },
 
-    prepend : function(arr, item) {
-
+    prepend: function(arr, item) {
+      arr.unshift(item);
+      return arr;
     },
 
-    curtail : function(arr) {
-
+    curtail: function(arr) {
+      arr.shift();
+      return arr;
     },
 
-    concat : function(arr1, arr2) {
-
+    concat: function(arr1, arr2) {
+      var combinedArray = [];
+      combinedArray = arr1.concat(arr2);
+      return combinedArray;
     },
 
-    insert : function(arr, item, index) {
-
+    insert: function(arr, item, index) {
+      arr.splice(index, 0, item);
+      return arr;
     },
 
-    count : function(arr, item) {
-
+    count: function(arr, item) {
+      var count = 0;
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+          count++;
+        }
+      }
+      return count;
     },
 
-    duplicates : function(arr) {
-
+    duplicates: function(arr) {
+      var duplicates = [];
+      // loop over array to grab first element
+      for (var i = 0; i < arr.length; i++) {
+        var element = arr[i];
+        // compare element to each element in array
+        for (var j = 0; j < arr.length; j++) {
+          // if the elements of each array are the same, are at different indices
+          // and are unique to duplicates array
+          if (arr[i] === arr[j] && i !== j && duplicates.indexOf(arr[i]) === -1) {
+            duplicates.push(arr[i]);
+          }
+        }
+      }
+      return duplicates;
     },
 
-    square : function(arr) {
-
+    square: function(arr) {
+      var squares = [];
+      for (var i = 0; i < arr.length; i++) {
+        squares.push(arr[i] * arr[i]);
+      }
+      return squares;
     },
 
-    findAllOccurrences : function(arr, target) {
-
+    findAllOccurrences: function(arr, target) {
+      // Find all indices of element in array
+      var indices = [];
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+          indices.push(i);
+        }
+      }
+      return indices;
     }
   };
 });
